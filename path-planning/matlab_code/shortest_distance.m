77b25b562bf909c57b5c4877af354ed107f99ea3
AAnirudh07 <anirudh19015@cse.ssn.edu.in>
2023-03-14 14:04:51 +0530

    modified distance metric code

--- path-planning/matlab_code/shortest_distance.m
+++ path-planning/matlab_code/shortest_distance.m
@@ -1,23 +1,35 @@
 function true_realdistance = shortest_distance(poin1, poin2, obstacl1)
-     pqx = poin2(1,1) - poin1(1,1);
-	 pqy = poin2(1,2) - poin1(1,2);
-	 pqz = poin2(1,3) - poin1(1,3);
-	 dx = obstacl1(1,1) - poin1(1,1);
-	 dy = obstacl1(1,2) - poin1(1,2);
-	 dz = obstacl1(1,3) - poin1(1,3);
-	 d_1 = pqx*pqx + pqy*pqy + pqz*pqz;  % qp线段长度的平方
-	 t_2 = pqx*dx + pqy*dy + pqz*dz;   % p pt向量 点积 pq 向量（p相当于A点，q相当于B点，pt相当于P点）
 
-		t_1 = t_2 / d_1;    %?此时t 相当于 上述推导中的 r。
+    % Calculate the difference between the two points in each dimension
+    pqx = poin2(1,1) - poin1(1,1);
+	pqy = poin2(1,2) - poin1(1,2);
+	pqz = poin2(1,3) - poin1(1,3);
+	dx = obstacl1(1,1) - poin1(1,1);
+	dy = obstacl1(1,2) - poin1(1,2);
+	dz = obstacl1(1,3) - poin1(1,3);
+
+    % Calculate the squared distance between the two points
+	d_1 = pqx*pqx + pqy*pqy + pqz*pqz;
+
+    % Calculate the projection of the obstacle point onto the line between the two given points
+	t_2 = pqx*dx + pqy*dy + pqz*dz;
+
+    % Calculate the value of t, which represents the relative position of the projection along the line
+	t_1 = t_2 / d_1;
+
+    % If t is less than 0, the projection falls outside the line segment and we take the distance to the first point
 	if (t_1 < 0)
-		t_1 = 0;  %?当t（r）< 0时，最短距离即为 pt点 和 p点（A点和P点）之间的距离。
+		t_1 = 0;
+    % If t is greater than 1, the projection falls outside the line segment and we take the distance to the second point
 	else if (t_1 > 1)
-		t_1 = 1; %  当t（r）> 1时，最短距离即为 pt点 和 q点（B点和P点）之间的距离。
+		t_1 = 1;
+    % If t is between 0 and 1, the projection falls inside the line segment and we take the distance to the line
         else
             t_1 = 2;
         end
     end
     
+    % Calculate the distance to the obstacle based on the value of t
     if t_1 == 0
         true_realdistance = Distance(poin1,obstacl1);
     end
@@ -32,21 +44,48 @@ end
 
 function realdistance = point_to_line(point1, point2, obstacle1)
 
-vx1x2 = point2 - point1;%向量x1到x2
-vx1x3 = obstacle1 - point1;%向量x1到x3[
-% if  vx1x2'* vx1x2 == 0 %如果x1与x2重合，注意，对浮点数采用等于0的判断不可靠
-%     realdistance = 0;
-% elseif vx1x3' * vx1x3 == 0%如果x1与x3重合，这种情况下距离是否为0需要自己定义
-%     realdistance = 0;
-% else
-     inner_product = dot(vx1x2, vx1x3);%两个向量内积
-     inner_product_2 = inner_product * inner_product;%内积平方
-     cos_2 = inner_product_2 / dot(vx1x2,vx1x2) / dot(vx1x3, vx1x3);%夹角cos值的平方
-     sin_2 = 1 - cos_2;  %夹角sin的平方
-     dis_2 = dot(vx1x3,vx1x3) * sin_2;
-     realdistance = sqrt(dis_2); %距离
+    % Calculate the vector between the two given points
+    vx1x2 = point2 - point1;
+
+    % Calculate the vector between the first point and the obstacle
+    vx1x3 = obstacle1 - point1;
+
+    % Calculate the inner product of the two vectors
+    inner_product = dot(vx1x2, vx1x3);
+
+    % Calculate the square of the inner product
+    inner_product_2 = inner_product * inner_product;
+
+    % Calculate the square of the cosine of the angle between the two vectors
+    cos_2 = inner_product_2 / dot(vx1x2,vx1x2) / dot(vx1x3, vx1x3);
+
+    % Calculate the square of the sine of the angle between the two vectors
+    sin_2 = 1 - cos_2;
+
+    % Calculate the square of the distance between the first point and the line connecting the two points
+    dis_2 = dot(vx1x3,vx1x3) * sin_2;
+
+    % Calculate the distance between the obstacle and the line connecting the two points
+    realdistance = sqrt(dis_2);
+
 end
 
-function dist = Distance(X,Y)
-     dist = sqrt(sum((X - Y).^2));
+function dist = Distance(X, Y)
+    % Calculate Chebyshev distance
+    dist_chebyshev = max(abs(X-Y));
+    
+    % Calculate Manhattan distance and count the number of non-aligned coordinates
+    non_aligned = find(X ~= Y);
+    dist_manhattan = sum(abs(X(non_aligned) - Y(non_aligned)));
+    num_non_aligned = length(non_aligned);
+    
+    % Calculate Minkowski distance
+    if num_non_aligned == 0
+        dist_minkowski = dist_chebyshev;
+    else
+        dist_minkowski = (dist_manhattan / num_non_aligned) ^ (num_non_aligned / (num_non_aligned - 1));
+    end
+    
+    % Return the maximum of Chebyshev and Minkowski distances
+    dist = max(dist_chebyshev, dist_minkowski);
 end
