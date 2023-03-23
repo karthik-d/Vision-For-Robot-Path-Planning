4ae5bdab927b0703bf5b4890ece579aa794d78eb
AAnirudh07 <anirudh19015@cse.ssn.edu.in>
2023-03-23 14:51:57 +0530

    path planning page + routes

--- app/app.py
+++ app/app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 #import matlab.engine
 
 app = Flask(__name__)
 
-
 @app.route('/',  methods=["GET", "POST"])
-def predict():
+def home():
     if request.method == 'POST':
         image_file = request.files['ct-image-file']
         try:
@@ -14,12 +13,22 @@ def predict():
         except:
             print('LOG: Unable to load image.')
 
-        # TODO - load the model and run inference
+        # TODO - load the model, run inference and save the output
 
-        return render_template('path-planning.html')
+        return redirect(url_for('predict'))
 
     else:
         return render_template('model-prediction.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        pass
+    
+    else:
+        # TODO - pass actual img filename
+        return render_template('set-params-path-planning.html', image = 'segmented-output-sample.png')
+
 
 if __name__ == "__main__":
     app.secret_key="secret123"
