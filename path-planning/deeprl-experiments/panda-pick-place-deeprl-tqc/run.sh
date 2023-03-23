53624a9bc92675b8299261fadcc1073c8c50f141
AAnirudh07 <anirudh19015@cse.ssn.edu.in>
2023-03-23 09:17:29 +0530

    add deeprl + tqc

---
+++ path-planning/deeprl-experiments/panda-pick-place-deeprl-tqc/run.sh
@@ -0,0 +1,5 @@
+# Download model and save it into the logs/ folder
+python -m rl_zoo3.load_from_hub --algo tqc --env PandaPickAndPlace-v1 -orga sb3 -f logs/
+python enjoy.py --algo tqc --env PandaPickAndPlace-v1  -f logs/
+
+python train.py --algo tqc --env PandaPickAndPlace-v1 -f logs/
