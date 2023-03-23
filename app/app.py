7524ccb02b70355c7173798c570b85480f206f66
AAnirudh07 <anirudh19015@cse.ssn.edu.in>
2023-03-23 12:51:55 +0530

    image loader

--- app/app.py
+++ app/app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 #import matlab.engine
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return render_template('model-prediction.html')
+@app.route('/',  methods=["GET", "POST"])
+def predict():
+    if request.method == 'POST':
+        image_file = request.files['ct-image-file']
+        try:
+            image_data = image_file.read()
+            print('LOG: Image data loaded!')
+        except:
+            print('LOG: Unable to load image.')
+
+        # TODO - load the model and run inference
+
+        return render_template('path-planning.html')
+
+    else:
+        return render_template('model-prediction.html')
 
 if __name__ == "__main__":
     app.secret_key="secret123"
