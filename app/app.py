672c09f02d26d3667db04bcf4e8b912b4464202f
AAnirudh07 <anirudh19015@cse.ssn.edu.in>
2023-04-23 22:25:18 +0530

    integrated v2 - matlab + input args

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import matlab.engine
 from skimage import io, transform
 # import pandas as pd
+import numpy as np
 import json
 import plotly
 import plotly.express as px
@@ -9,6 +10,10 @@ import os
 
 from figures import preparator, sequencer
 from config import *
+
+
+TARGET = []
+OBSTACLE = []
 
 app = Flask(__name__)
 
@@ -27,31 +32,42 @@ def home():
         return redirect(url_for('predict'))
 
     else:
-        return render_template('model-prediction.html')
+        return render_template('result-visualization.html')
 
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
+        # Get the values for target and obstacle from the form
+        target = [request.form.get(f'target{i}') for i in range(1, 4)]
+        obstacle = [request.form.get(f'obstacle{i}') for i in range(1, 4)]
+        
+        # Convert the values to integers
+        target = [int(x) for x in target]
+        obstacle = [int(x) for x in obstacle if x != '']
+        
+        # Print the values for testing
+        print("Target:", target)
+        print("Obstacle:", obstacle)
+
+        global TARGET, OBSTACLE
+        TARGET = target
+        OBSTACLE = obstacle
+         
         return redirect(url_for('pathplanning'))
     
     else:
         # TODO - add model
-        print("ads")
         volume_container = sequencer.VolumeSliceSequencer(
             volume_path = os.path.join('assets', 'scan_6'), 
             mask_path = os.path.join('assets', 'pred_6'), 
             # gt_path = os.path.join('assets', 'gt_6'), 
             target_slice_size = (SLICE_WIDTH, SLICE_HEIGHT)
         )
-        print("ads")
         volume_container.iter_with_mask()
         # volume_container.iter_with_gt()
-        print("ads")
         fig = preparator.get_volume_figure(volume_container)
-        print("ads")
         graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-        print("ads")
 
         return render_template('set-params-path-planning.html', graphJSON=graphJSON)
 
@@ -60,20 +76,34 @@ def predict():
 def pathplanning():
 
     if request.method == 'POST':
+        global TARGET
+
         # Load the engine and run the path-planning files
         eng = matlab.engine.start_matlab()
-        eng.run("startup_rvc.m", nargout=0)            
-        eng.run("C:/Users/aniru/Vision-For-Robot-Path-Planning/path-planning/matlab-code/Pose_Schedule_with_Reschedule.m", nargout=0)    
+        eng.addpath('C:/Users/aniru/Vision-For-Robot-Path-Planning/path-planning/matlab-code')
+
+        eng.startup_rvc(nargout=0)            
+        eng.Pose_Schedule_with_Reschedule(matlab.double(TARGET), nargout=0)
+
+        # Get a list of all running MATLAB engines
+        matlab_engines = matlab.engine.find_matlab()
+
+        # Keep the engine running until the user explicitly closes it
+        while len(matlab_engines) > 0:
+            matlab_engines = matlab.engine.find_matlab()    
         
         # Wait for user input before closing the figures
-        input('Press Enter to Continue: ')
+        #input('Press Enter to Continue: ')
 
-        return render_template('path-planning.html', loading = False)
+        #return render_template('path-planning.html', loading = False)
 
     else:
         # Loading page
         return render_template('path-planning.html', loading = True)
 
+@app.route('/complete')
+def complete():
+    return render_template('complete.html')
 
 if __name__ == "__main__":
     app.secret_key="secret123"
